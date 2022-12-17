a6bab9c4d0668d537a9dd5600c485b101f36537a
Irena Daneva <de.luxe9997@gmail.com>
2022-12-17 21:04:56 +0200

    Update pastryShop.java

--- pastryShop.java
+++ pastryShop.java
@@ -17,8 +17,8 @@ Until December 22 inclusive, the following discounts exist, which are applied in
 
 3 lines are read from the console:
 1. Pastry - text - "Cake", "Souffle" or "Baklava"
-2. Number of sweets ordered - an integer in the interval [1 … 10000]
-3. Day of the month of December before Christmas - an integer in the interval [1 … 24]
+2. Number of sweets ordered - an integer in the interval 
+3. Day of the month of December before Christmas - an integer in the interval 
 
 1 line is printed to the console: The price to be paid, formatted to the second decimal place */
 
@@ -28,8 +28,10 @@ public class pastryShop
 {
     public static void main(String[] args)
     {
+        //new scanner obj
         Scanner scan = new Scanner(System.in);
-
+        
+        //pastry prices
         double cakeBefore = 24;
         double cakeAfter = 28.7;
         double souffleBefore = 6.66;
@@ -38,21 +40,24 @@ public class pastryShop
         double baklavaAfter = 16.98;
         double finalPrice = 0;
 
+        //input kind/ number of pastry to buy and day of month
         String pastry = scan.nextLine();
         int total = Integer.parseInt(scan.nextLine());
         int days = Integer.parseInt(scan.nextLine());
-
+        
+        //switch case for pastry kind
         switch (pastry) {
             case "Cake" -> {
+                //if bought before 15 December
                 if (days <= 15) {
                     double price = cakeBefore * total;
                     if (price > 200) {
-                        //25% discount
+                        //25% discount for total price over 200
                         price = price * 0.75;
                         finalPrice = price;
                     }
                     else if (price <= 200 && price > 100) {
-                        //15% discount
+                        //15% discount for price between 100 and 200
                         price = price * 0.85;
                         finalPrice = price;
                     }
@@ -60,16 +65,17 @@ public class pastryShop
                     {
                         finalPrice = price;
                     }
+                    //10% discount of total price before 15 December
                     finalPrice = finalPrice * 0.9;
                 } else if (days <= 22) {
                     double price = cakeAfter * total;
                     if (price > 200) {
-                        //25% discount
+                        //25% discount for total price over 200
                         price = price * 0.75;
                         finalPrice = price;
                     }
                     else if (price <= 200 && price > 100) {
-                        //15% discount
+                        //15% discount for price between 100 and 200
                         price = price * 0.85;
                         finalPrice = price;
                     }
@@ -85,12 +91,12 @@ public class pastryShop
                 if (days <= 15) {
                     double price = souffleBefore * total;
                     if (price > 200) {
-                        //25% discount
+                        //25% discount for total price over 200
                         price = price * 0.75;
                         finalPrice = price;
                     }
                     else if (price <= 200 && price > 100) {
-                        //15% discount
+                        //15% discount for price between 100 and 200
                         price = price * 0.85;
                         finalPrice = price;
                     }
@@ -98,16 +104,17 @@ public class pastryShop
                     {
                         finalPrice = price;
                     }
+                    //10% discount of total price before 15 December
                     finalPrice = finalPrice * 0.9;
                 } else if (days <= 22) {
                     double price = souffleAfter * total;
                     if (price > 200) {
-                        //25% discount
+                        //25% discount for total price over 200
                         price = price * 0.75;
                         finalPrice = price;
                     }
                     else if (price <= 200 && price > 100) {
-                        //15% discount
+                        //15% discount for price between 100 and 200
                         price = price * 0.85;
                         finalPrice = price;
                     }
@@ -123,12 +130,12 @@ public class pastryShop
                 if (days <= 15) {
                     double price = baklavaBefore * total;
                     if (price > 200) {
-                        //25% discount
+                        //25% discount for total price over 200
                         price = price * 0.75;
                         finalPrice = price;
                     }
                     else if (price <= 200 && price > 100) {
-                        //15% discount
+                        //15% discount for price between 100 and 200
                         price = price * 0.85;
                         finalPrice = price;
                     }
@@ -136,16 +143,17 @@ public class pastryShop
                     {
                         finalPrice = price;
                     }
+                    //10% discount of total price before 15 December
                     finalPrice = finalPrice * 0.9;
                 } else if (days <= 22) {
                     double price = baklavaAfter * total;
                     if (price > 200) {
-                        //25% discount
+                        //25% discount for total price over 200
                         price = price * 0.75;
                         finalPrice = price;
                     }
                     else if (price <= 200 && price > 100) {
-                        //15% discount
+                        //15% discount for price between 100 and 200
                         price = price * 0.85;
                         finalPrice = price;
                     }
@@ -158,6 +166,8 @@ public class pastryShop
                 }
             }
         }
+        
+        //print final price
         System.out.printf("%.2f", finalPrice);
     }
 }
